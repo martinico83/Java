97a5a547226847431af678bcbefa50cf3e5fc44e
martinico83 <martin.nicolas.os83@gmail.com>
2021-09-09 22:41:40 +0000

    modifique REPLIT

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 // Mi clase en Java
 public class Main{
 public static void main (String args[]){
-  System.out.println("HOLA MUNDO DESDE JAVA");
+  System.out.println("HOLA MUNDO DESDE REPLIT");
 }
 }
