16fff12688d4f5fb921faa6e25992eef475e7455
martinico83 <martin.nicolas.os83@gmail.com>
2021-09-11 19:56:56 -0300

    Concatenacion, particularidades de la concatenacion y VAR

--- NB_HolaMundoJava/src/HolaMundo.java
+++ NB_HolaMundoJava/src/HolaMundo.java
@@ -1,8 +1,25 @@
 
 //Mi clase en Java
 public class HolaMundo {
-    public static void main (String args[]){
-        System.out.println("¡¡HOLA MUNDO DE LOCOS!!");
+
+    public static void main(String args[]) {
+        var palabra = "HOLA TINO";
+        System.out.println(palabra);
+        var conComa = 15.699;
+        System.out.println(conComa);
+        System.out.println(palabra + " " + conComa);
+        var nombre = "Martin";
+        var segundoNombre = "Nicolas";
+        var apellido = "Orona";
+        var segundoApellido = "Sacido";
+        var nombreCompleto = nombre + " " + segundoNombre + " " + apellido + " " + segundoApellido;
+        System.out.println(nombreCompleto);
+        var num1 = 1;
+        var num2 = 8;
+        System.out.println(num1 + num2 + nombre);
+        System.out.println(nombre + num1 + num2);
+        System.out.println(nombre + (num1 + num2));
+
     }
-    
+
 }
