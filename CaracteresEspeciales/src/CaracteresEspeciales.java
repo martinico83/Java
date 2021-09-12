1d59aaa542f5ea48eccef804d9152b13854311f2
martinico83 <martin.nicolas.os83@gmail.com>
2021-09-11 21:56:53 -0300

    Caracteres especiales

---
+++ CaracteresEspeciales/src/CaracteresEspeciales.java
@@ -0,0 +1,12 @@
+
+public class CaracteresEspeciales {
+    public static void main(String args[]){
+        var nombre="Martin";
+        System.out.println("Salto de linea: \n" + nombre);
+        System.out.println("Tabulador: \t" + nombre);
+        System.out.println("Retroceso: \b" + nombre);
+        System.out.println("Comilla simple: '" + nombre + "'");
+        System.out.println("Comilla doble: \"" +nombre+ "\"");
+    }
+    
+}
