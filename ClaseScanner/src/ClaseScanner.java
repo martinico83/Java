d59942c3ddccd4e8afc41d10c0c61f6193d256a1
martinico83 <martin.nicolas.os83@gmail.com>
2021-09-12 22:38:19 -0300

    Empesando ClaseScaner, pero no vi el video todabia

---
+++ ClaseScanner/src/ClaseScanner.java
@@ -0,0 +1,6 @@
+
+public class ClaseScanner {
+    public static void main(String args[]){
+        System.out.println("Hola mundo");
+    }
+}
--- Main.java
+++
@@ -1,6 +0,0 @@
-// Mi clase en Java
-public class Main{
-public static void main (String args[]){
-  System.out.println("HOLA MUNDO DESDE REPLIT");
-}
-}
