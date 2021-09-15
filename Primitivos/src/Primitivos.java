234c7a8cbbc8ed9595a8e86e830ea476cd09508e
martinico83 <martin.nicolas.os83@gmail.com>
2021-09-14 21:18:55 -0300

    Primitivos

---
+++ Primitivos/src/Primitivos.java
@@ -0,0 +1,17 @@
+
+
+//Mi clase en Java
+public class Primitivos {
+
+    public static void main(String args[]) {
+        /*
+            tipos primitivos enteros: byte, short, int, long
+         */
+        byte numeroByte = (byte) 129;
+        System.out.println("Valor byte:" + numeroByte);
+        System.out.println("Valor minimo byte:" + Byte.MIN_VALUE);
+        System.out.println("Valor maximo byte:" + Byte.MAX_VALUE);
+
+      
+    }
+}
