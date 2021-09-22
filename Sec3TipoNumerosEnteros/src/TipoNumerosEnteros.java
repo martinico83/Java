ca0166c1ff7d34b01eecb3873274b867e59cd269
martinico83 <martin.nicolas.os83@gmail.com>
2021-09-22 20:48:08 -0300

    Tipo numericos enteros

---
+++ Sec3TipoNumerosEnteros/src/TipoNumerosEnteros.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author martin
+ */
+public class TipoNumerosEnteros {
+
+    public static void main(String args[]) {
+       
+        short numeroShort = 10;
+        System.out.println("numeroShort = " + numeroShort);
+        System.out.println("El valor minimo de short es = " + Short.MIN_VALUE);
+        System.out.println("El valor maximo de short es = " + Short.MAX_VALUE);
+        
+        int numeroInt = 10;
+        System.out.println("numeroInt = " + numeroInt);
+        System.out.println("El valor minimo de int es = " + Integer.MIN_VALUE);
+        System.out.println("El valor maximo de int es = " + Integer.MAX_VALUE);
+        
+        long numeroLong = 20;
+        System.out.println("numeroLong = " + numeroLong);
+        System.out.println("El valor minimo de long es = " + Long.MIN_VALUE);
+        System.out.println("El valor maximo de long es = " + Long.MAX_VALUE);
+        
+    }
+
+}
