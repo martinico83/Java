7ebe2eadcdef8d50270b6085f5a8804902b3202c
martinico83 <martin.nicolas.os83@gmail.com>
2021-09-24 20:23:32 -0300

    Flotantes

---
+++ Sec3PrimiFlot/src/PrimiFlot.java
@@ -0,0 +1,16 @@
+
+public class PrimiFlot {
+
+    public static void main(String args[]) {
+        float numeroFloat = (float) 10.0;
+        System.out.println("numeroFloat = " + numeroFloat);
+        System.out.println("El valor minimo de float es: " + Float.MIN_VALUE);
+        System.out.println("El valor maximo de float es: " + Float.MAX_VALUE);
+
+        double numeroDouble = 10.33;
+        System.out.println("numeroDouble = " + numeroDouble);
+        System.out.println("El valor minimo de double es: " + Double.MIN_VALUE);
+        System.out.println("El valor maximo de double es: " + Double.MAX_VALUE);
+    }
+
+}
